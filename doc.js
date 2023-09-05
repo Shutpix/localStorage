6ae22e13fd54ac101fe687daf6eef7485ab8f9d2
Shutpix <vishalmishra1249@gmail.com>
2023-09-05 15:42:23 +0530

    2nd commit

--- doc.js
+++ doc.js
@@ -2,13 +2,28 @@ function svaeToLocalStorage(event)
 {
     event.preventDefault();
     const name =  event.target.username.value;
-    const email =  event.target.emailId.value;
-    const phonenumber = event.target.phoneNumber.value;
-    localStorage.setItem('name',name );
-    localStorage.setItem('email',email );
-    localStorage.setItem('phonenumber',phonenumber );
+    const email =  event.target.emailid.value;
+    const phonenumber = event.target.phonenumber.value;
+   // localStorage.setItem('name',name );
+  //localStorage.setItem('email',email );
+ // localStorage.setItem('phonenumber',phonenumber );
 
+ const obj = {
+    name,
+    email,
+    phonenumber
 
+ }
 
+ localStorage.setItem(obj.JASON.stringify(obj))
+ showUserOnScreen(obj);
 
+}
+
+function showUserOnScreen(obj)
+{
+    const parentElem = document.getElementById('listOfitms')
+    const childElem = document.createElement('li')
+    childElem.textContent = obj.name + ' - ' + obj.email + ' - ' + obj.phonenumber
+    parentElem.appendChild(childElem);
 }
